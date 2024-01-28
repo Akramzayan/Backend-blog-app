0903142e7e28537d870a6749a676703ab9c0836a
akram <akremzayen@gmail.com>
2024-01-28 17:46:51 +0100

    User Api Done

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import { Schema,model } from "mongoose";
+const UserSchema = new Schema ({
+    avatar:{
+        type:String,
+        default:"",
+    },
+    name:{type:String,
+        required:true},
+    email:{type:String,
+        required:true},
+    password:{
+        type:String,
+        required:true},
+    verified:{
+        type:Boolean,
+        default:false},
+    verifactionCode:{
+        type:String,
+        required:false
+    },
+    admin:{
+        type:Boolean,
+        default:false
+    }
+    
+    },{timestamps:true});
+
+    const User =model("User",UserSchema);
+    export default User;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectDB from "./config/db";
+//Routes
+import userRoutes from "./routes/userRoutes.js";    
 
 
 
@@ -20,6 +22,8 @@ app.use(express.json());
 app.get("/",(req,res) => {
     res.send("Server is Running")
 });
+app.use('/api/users',userRoutes);
+
 app.listen(process.env.PORT,() => {
     console.log(`Server is Running On Port ${process.env.PORT}`);
 
