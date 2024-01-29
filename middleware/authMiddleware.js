de1186ad71d31f86eca29fa987e010bca628ef19
akram <akremzayen@gmail.com>
2024-01-29 17:27:57 +0100

    AuthMiddleware Done

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import {verify} from "jsonwebtoken";
+import User from "../models/User";
+export const authGuard = async(req,res,next) => {
+    if(req.headers.authorization && req.headers.startsWith("Bearer")){
+        try {
+            const token = req.headers.authorization.split(" ")[1];
+            const {id} = verify(token,process.env.JWT_TOKEN);
+            req.user = await User.findById(id).select("-password");
+            next();
+        } catch (error) {
+            let err = new Error("Not Authorized, Token Failed");
+            err.statusCode = 401;
+            next(err);
+        }
+    }
+    else{
+        let error = new Error("Not Authorized, No Token Found");
+        error.statusCode=401;
+        next(error);
+
+    }
+}
